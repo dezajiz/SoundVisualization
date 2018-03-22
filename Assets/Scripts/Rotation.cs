7af9bab9d1016f1ba6c8c37e8dff10af5588f917
kato takeshi <tkskto@gmail.com>
2018-03-23 07:35:00 +0900

    import tween

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Rotation : MonoBehaviour {
 
 	float rotateX;
 	float rotateY;
 	float rotateZ;
+	private Tweener tween; 
 	
 	void Start () {
 		rotateX = Random.Range(1, 360);
@@ -19,5 +21,15 @@ public class Rotation : MonoBehaviour {
 		rotateX += 2;
 		rotateY += 2;
 		rotateZ += 2;
+
+		if (Input.GetKey(KeyCode.Space)) {
+			if (tween != null) {
+				tween.Kill();
+			}
+			int scale = Mathf.FloorToInt(Random.Range(1, 3));
+			transform.localScale = new Vector3(scale, scale, scale);
+
+			tween = transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), 0.3f);
+		}
 	}
 }
