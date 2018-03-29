4cb7659d60ee93ee6edea18b26144eaacf3185d1
kato takeshi <tkskto@gmail.com>
2018-03-30 06:49:26 +0900

    位置調整必要

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class AddCube : MonoBehaviour {
+using DG.Tweening;
+public class Cube : MonoBehaviour {
 
 	public GameObject prefab;
 	public int CubeNum;
-
+	public int _x;
+	public int _y;
+	public int _z;
 	float rotateY = 0;
 	float RADIAN = Mathf.PI / 180;
+	Vector3[] cubePositions;
 
 	void Start () {
 		for (int i = 0; i < CubeNum; i++) {
@@ -25,10 +28,32 @@ public class AddCube : MonoBehaviour {
 			GameObject instance = Instantiate (prefab, pos, Quaternion.identity);
 			instance.transform.parent = gameObject.transform;
 		}
+
+		cubePositions = new Vector3[CubeNum];
+		int length = 10;
+
+		for (int x = 0; x < _x; x++) {
+			for (int y = 0; y < _y; y++) {
+				for (int z = 0; z < _z; z++) {
+					cubePositions[x * _x * 2 + y * _y + z] = new Vector3(x,y,z);
+				}
+			}
+		}
 	}
 	
 	void Update () {
 		gameObject.transform.rotation = Quaternion.Euler(0, rotateY, 0);
 		rotateY ++;
+
+		if (Input.GetKey(KeyCode.M)) {
+			Move();
+		}
+	}
+
+	void Move () {
+		for (int i = 0; i < CubeNum; i++) {
+			Transform child = transform.GetChild(i);
+			child.transform.DOMove(cubePositions[i], 0.3f);
+		}
 	}
 }
